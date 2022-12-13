b3c8842c93b0f66ca874ac87ed5d31486536432b
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2022-12-13 13:45:38 -0600

    making samples
    an array of strings to delete...

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,10 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface PlanetResponseNames {
+  next: string;
+  results: { name: string; }[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
 
   constructor(private httpSvc: HttpClient) { }
+
+  loadPlanets = () => {
+    const p1 = this.httpSvc.get<PlanetResponseNames>("https://swapi.dev/api/planets");
+    return p1;
+  };
 }
