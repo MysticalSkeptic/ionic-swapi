92103950016a57855e0de0dfcdbaef7d3846a7dc
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2022-12-13 14:13:09 -0600

    finished

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { concat, expand, repeat, EMPTY } from 'rxjs';
 
 export interface PlanetResponseNames {
   next: string;
@@ -14,7 +15,15 @@ export class SwapiService {
   constructor(private httpSvc: HttpClient) { }
 
   loadPlanets = () => {
+    // const p1 = this.httpSvc.get<PlanetResponseNames>("https://swapi.dev/api/planets");
+    // const p2 = this.httpSvc.get<PlanetResponseNames>("https://swapi.dev/api/planets?page=2");
+    // return concat(p1, p2);
+
     const p1 = this.httpSvc.get<PlanetResponseNames>("https://swapi.dev/api/planets");
-    return p1;
+    return p1.pipe(
+      // repeat(6)
+      expand(x => x.next ? this.httpSvc.get<PlanetResponseNames>(x.next) : EMPTY)
+    );
+    
   };
 }
