69095a019161a5b0a6f54aa2a5b5a18bcb866692
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2022-12-13 13:52:25 -0600

    got the observable going
    now lists and counts first group of planets

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,10 +10,19 @@ export class HomePage implements OnInit {
 
   constructor(private swapiSvc: SwapiService) {}
 
-  planets: string[] = ["Lord", "Glablok"];
+  planets: string[] = [];
 
   ngOnInit(): void {
-    
+    this.swapiSvc.loadPlanets().subscribe({
+      next: data => {
+        this.planets = [
+          ...this.planets,
+          ...data.results.map(x => x.name)
+        ];
+      },
+      error: err => {console.error(err);},
+      
+    });
   }
 
 }
